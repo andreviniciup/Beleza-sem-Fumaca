921021f2dfbe875ecbce7670f037d96a58c9e5d2
vinicius <andreviniciusrp@gmail.com>
2025-08-25 17:27:23 -0300

    agora vai2222

--- config.py
+++ config.py
@@ -5,12 +5,8 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua_chave_secreta_aqui_mude_para_producao'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///quiz.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///quiz.db'  # Usar SQLite por padrão
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # Configurações para PostgreSQL
-    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
 
 class DevelopmentConfig(Config):
     DEBUG = True
